1dcd245efd1593d6ea09f2f8626974e11d7e0722
Josh Pigford <josh@joshpigford.com>
2024-01-20 21:59:34 -0600

    Transition articles back to main database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = ButterCMS::Post.all({:page_size => 50})
+    @articles = Article.order('publish_at DESC')
     @title = "Articles"
   end
 
   def show
-    @article = ButterCMS::Post.find(params[:id])
+    @article = Article.where(slug: params[:id]).first
     @title = @article.title
+
+    if @article.nil?
+      redirect_to '/404'
+    end
   end
 end
